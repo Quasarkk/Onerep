3f245ab96b313e9ff76bc1366224223c896ada68
Tekz00r <elderscry@gmail.com>
2023-07-04 16:30:29 +0200

    Initial commit

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,10 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Welcome from '@/Components/Welcome.vue';
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <h1>AAAAAAAAAH</h1>
+    </AppLayout>
+</template>
---
+++ resources/js/Pages/Stats.vue
@@ -0,0 +1,16 @@
+<template>
+    <Applayout>
+        <div>
+            <canvas id="myChart" width="400" height="400"></canvas>
+        </div>
+    </Applayout>
+</template>
+
+<script>
+export default {
+    name: 'Stats',
+    mounted() {
+        console.log('Component mounted.')
+    },
+}
+</script>
