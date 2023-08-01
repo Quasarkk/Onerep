a9281af73ad505bfdba91ac85da215917a0973e2
Tekz00r <elderscry@gmail.com>
2023-08-01 11:45:11 +0200

    Restored the user related files
    Restored the migrations files related to users
    Remade the UserSeeder file
    Remade a migration + seed
    
    Thoses files appeared to be more useful than expected

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,16 +14,19 @@ class UserSeeder extends Seeder
     {
         DB::table('users')->insert([
             'name' => 'pascal',
+            'email' => 'pascal@gmail.com',
             'password' => Hash::make('motdepascal'),
         ]);
 
         DB::table('users')->insert([
             'name' => 'vincent',
+            'email' => 'vincent@gmail.com',
             'password' => Hash::make('motdevincent'),
         ]);
 
         DB::table('users')->insert([
             'name' => 'clement',
+            'email' => 'clement@gmail.com',
             'password' => Hash::make('motdeclement'),
         ]);
     }
